119ab70c4faeaed03cd679308c30189d44ea74ef
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-08-12 00:09:51 +0530

    Page Object Model Sample

--- 005-PageObjectModelTest.cs
+++ 005-PageObjectModelTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -6,19 +7,41 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 
 namespace CSharpSeleniumTraining {
+
+    public class IndexPage : TestAutomationBase {
+
+        private IWebDriver _driver;
+        private IWebElement _userName;
+        private IWebElement _password;
+        private IWebElement _signIn;
+
+        public IndexPage (IWebDriver driver) {
+            _driver = driver;
+            _userName = _driver.FindElement (By.Id ("text"));
+            _password = _driver.FindElement (By.Id ("password"));
+            _signIn = _driver.FindElement (By.Id ("login-button"));
+        }
+
+        public void Login (string username, string password) {
+            _userName.SendKeys (username);
+            _password.SendKeys (password);
+            _signIn.Click ();
+        }
+
+        public void Close () {
+            _driver.Quit ();
+        }
+    }
+
     class PageObjectModelTest : TestAutomationBase {
 
         static void Main (string[] args) {
             IWebDriver driver = new ChromeDriver (Consts.WebDriverLocation);
             driver.Navigate ().GoToUrl ("http://webdriveruniversity.com/Login-Portal/index.html?");
-            IWebElement  userName =  driver.FindElement (By.Id ("text"));
-            IWebElement password  =  driver.FindElement (By.Id ("password"));
-            IWebElement loginButton = driver.FindElement (By.Id ("login-button"));
-            userName.SendKeys ("username" );
-            password.SendKeys ("password" ); 
-            loginButton.Click ();
+            IndexPage indexPage = new IndexPage (driver);
+            indexPage.Login ("username", "passwprd");
             Thread.Sleep (5000);
-            driver.Quit ();
+            indexPage.Close ();
         }
     }
 }
