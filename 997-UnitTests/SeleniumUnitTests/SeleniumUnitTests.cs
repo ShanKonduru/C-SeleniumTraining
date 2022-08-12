a774e490194e3c6abe0f1c54fa8243f593f3d4ea
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-08-12 07:11:20 +0530

    added blank Unit Test Project

---
+++ 997-UnitTests/SeleniumUnitTests/SeleniumUnitTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumUnitTests
+{
+    [TestClass]
+    public class SeleniumUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
